17512888fc371ff061ad35c66d91b3407e89a0bc
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-02 11:29:43 +0300

    string interpolation

--- variables.dart
+++ variables.dart
@@ -4,6 +4,8 @@ void main(){
   final age=25;
   print(age);
   const isOpen=true;
-  isOpne= false;
+  // isOpne= false;
   print(isOpen);
+  print("my name is $name i am ${age+10}, it is $isOpen i am a male");
+
 }
