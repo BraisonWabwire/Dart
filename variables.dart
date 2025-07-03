b8df1203322c35d4cd10b56048935da64dd3c84c
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-03 10:11:42 +0300

    Asyc and aswit

--- variables.dart
+++ variables.dart
@@ -1,53 +1,24 @@
-// Learning classes
+// Asyc, await, future
+
 void main(){
-   
-   
-  //  var noodless=Pizza(["camdel","shbueb"], "vegVolacano", 19.5);
-  //  print(noodless.format());
+  fetchPost().then((p){
+    print(p.title);
+    print(p.UserId);
+  });
 
-   var food=collection<MenuItem>("Skuma wiki", ["hello","hi","yoh"]);
-   var random=food.randomItem();
-   print(random);
 }
-class MenuItem{
+Future<Post> fetchPost(){
+  const delay=Duration(seconds: 3);
+
+  return Future.delayed(delay,(){
+      return Post('mypost', 123);
+  });
+}
+
+class Post{
+
   String title;
-  double price;
-  double tax=0.23;
-  
-  MenuItem(this.title, this.price);
-  double get finalPrice=>price*tax;
+  int UserId;
 
-  format(){
-    return "$title,---->$price,---->$finalPrice";
-  }
-}
-
-class Pizza extends MenuItem{
-  List<String> toppings;
-
-  Pizza(this.toppings, super.title,super.price);
-
-  @override
-  String format(){
-    var formatToppings = "contains:";
-    
-    for (final t in toppings){
-      formatToppings='$formatToppings $t';
-    }
-    return '$title->$price \n$formatToppings';
-
-  }
-}
-
-class collection<T>{
-  String name;
-  List<T> data;
-
-  collection(this.name,this.data);
-
- T randomItem(){
-    data.shuffle();
-    return data[0];
-  }
-
+  Post(this.title, this.UserId);
 }
