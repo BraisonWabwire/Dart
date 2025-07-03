131d2334a1f0813f0a53a68d12f40f762cf6ff00
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-07-03 11:27:26 +0300

    HTTP response

--- my_project/bin/my_project.dart
+++ my_project/bin/my_project.dart
@@ -1,25 +1,33 @@
 // import 'package:my_project/my_project.dart' as my_project;
 
 // Asyc, await, future
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
 
 void main() async{
-  final post=await fetchPost();
+  final post= await fetchPost();
   print(post.title);
-  print(post.UserId);
+  print(post.userId);
 
 }
-Future<Post> fetchPost(){
-  const delay=Duration(seconds: 3);
+Future<Post> fetchPost() async {
+  var url = Uri.https('jsonplaceholder.typicode.com', '/posts/1');
 
-  return Future.delayed(delay,(){
-      return Post('mypost', 123);
-  });
-}
+  var response = await http.get(url);
+
+  // print(response.body);
+  print('Status Code: ${response.statusCode}');
+  Map <String, dynamic> data=convert.jsonDecode(response.body);
+
+  return Post(data["title"],data["UserID"]);
+
+} 
 
 class Post{
 
   String title;
-  int UserId;
+  int userId;
 
-  Post(this.title, this.UserId);
+  Post(this.title, this.userId);
 }
